e3e82d070622a372df32f9e18360541895e1deac
Gizzelle Harris <harrigiz000@warren.k12.in.us>
2024-02-06 08:16:44 -0500

    commit

---
+++ 06_Types, Modules, and Packages/02_Modules, Import and Math/assignment.py
@@ -0,0 +1,9 @@
+
+leg1 = float(input("Enter an integer: "))
+leg2 = float(input("Enter another integer: "))
+print((leg1, leg2))
+
+import math
+hyp = print(math.hypot(leg1, leg2))
+
+print("The Hypotenuse is", hyp)
