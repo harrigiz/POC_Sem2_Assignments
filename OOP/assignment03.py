3697169739329ec4e1af5164559160ff0fa2602c
Gizzelle Harris <harrigiz000@warren.k12.in.us>
2024-03-06 08:17:36 -0500

    commit

--- OOP/assignment03.py
+++ OOP/assignment03.py
@@ -14,10 +14,20 @@ class Rectangle:
         return 2 * self.__base + 2 * self.__height
     
     def get_area(self) -> float:
-        return self._area
-    #Youdo get_area method
+        return self.__area
  
  
  
 #YOUDO>  create two rectangles.  print their base, height, perimeter, and area
 #using only the methods not the fields/property/attributes
+Rectangle1 = Rectangle
+print("The base of Rectangle1 is", Rectangle1.get_base())
+print("The height of Rectangle1 is", Rectangle1.get_height())
+print("The perimeter of Rectangle1 is", Rectangle1.get_perimeter())
+print("The area of Reactangle1 is", Rectangle1.get_area())
+
+rectangle2 = Rectangle
+print("The base of Rectangle1 is", rectangle2.get_base())
+print("The height of Rectangle1 is", rectangle2.get_height())
+print("The perimeter of Rectangle1 is", rectangle2.get_perimeter())
+print("The area of Reactangle1 is", rectangle2.get_area())
