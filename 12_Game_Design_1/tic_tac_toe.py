5dbbe16b375e087386a34eca49f620ab7a5334c1
Gizzelle Harris <harrigiz000@warren.k12.in.us>
2024-04-12 07:46:19 -0400

    commit

--- 12_Game_Design_1/tic_tac_toe.py
+++ 12_Game_Design_1/tic_tac_toe.py
@@ -1,35 +1,55 @@
 grid = [
-    ["-","-","-"],
-    ["-","-","-"],
-    ["-","-","-"]
+    ["1", "2", "3"],
+    ["4", "5", "6"],
+    ["7", "8", "9"],
 ]
 
-#Prints game grid
+current_player = "X"
+
 def print_grid():
-    for number in range(len(grid)):
-        print(grid[number][0], end="")
-        print("|", end="")
-        print(grid[number][1], end="")
-        print("|", end="")
-        print(grid[number][2])
-        if (number !=2):
-               print("*****")
-            
-#Prints inro to game
-def game_intro():
-    print("Welcom to TIC-TAC-TOE!!")
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+            if col != 2:
+                print(grid[row][col], end="")            
+            else:
+                print(grid[row][col])
+                
+def handle_input(value):
+    if(value.__eq__("STOP")):
+        return False
+    elif(value.__eq__("1")):
+        return False
+    elif (value.__eq__("2")):
+        return False
+    elif (value.__eq__("3")):
+        return False
+    elif(value.__eq__("4")):
+        return False
+    elif (value.__eq__("5")):
+        return False
+    elif(value.__eq__("6")):
+        return False
+    elif(value.__eq__("7")):
+        return False
+    elif(value.__eq__("8")):
+        return False
+    elif (value.__eq__("9")):
+        return False
+    else:
+        return True
+        
 
-#Game Loop
 def game_loop():
-
-    game_intro()
-
+    print("Welcome to TIC TAC TOE")
+    user_choice = ""
     while(True):
-        user_request = input("Enter STOP to end the game, or anything else to continue.")
-        if user_request.__eq__("STOP"):
+        print_grid()
+        while (handle_input(user_choice)):
+            user_choice = input("Enter STOP to end.  Or a number (1-9) where to put the piece: ")
+        if user_choice.__eq__("STOP"):
             break
-        print("loop running!")
-    
-    print("Game Over")
-
+        print("Game Loop Running")
+        user_choice = ""
+    print("GAME OVER")
+        
 game_loop()
