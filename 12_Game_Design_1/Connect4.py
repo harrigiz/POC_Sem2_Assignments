b7f5848a2a15e4adfec942c74f582e0cb9f60475
Gizzelle Harris <harrigiz000@warren.k12.in.us>
2024-04-19 07:45:49 -0400

    commit

--- 12_Game_Design_1/Connect4.py
+++ 12_Game_Design_1/Connect4.py
@@ -8,7 +8,7 @@ grid = [
     ["36", "37","38","39","40","41","42"]
 ]
 
-current_pieces = "R", "Y"
+current_piece = "R", "Y"
 
 def print_grid():
     for row in range(len(grid)):
@@ -18,6 +18,11 @@ def print_grid():
             else:
                 print(grid[row][col])
                 print()
+
+def is_bad_choice_string(choice : str):
+    if (choice.isnumeric() and int(choice) >= 1 and int(choice) <= 9):
+        return False
+    return True
 
 def get_row(grid_spot):
     if grid_spot == 1 or grid_spot == 2 or grid_spot == 3:
@@ -83,7 +88,82 @@ def get_col(grid_spot):
     else:
         return     
 
-#********************************************************************************************************
+def place_piece(grid_spot : int):
+    while(True):
+        row = get_row(grid_spot)
+        col = get_col(grid_spot)
+        grid_value = grid[row][col]
+        if (not grid_value.__eq__("R") and not grid_value.__eq__("Y")):
+            break
+        user_choice = ""
+        while (is_bad_choice_string(user_choice)):
+            user_choice = input(
+                "Enter a number (1-42) where to put the piece: ")
+        grid_spot = int(user_choice)
+    grid[row][col] = current_piece 
+
+def check_row():
+    for row in range(4):
+        if grid[row][0].__eq__(grid[row][1]) and grid[row][1].__eq__(grid[row][2]):
+            return True
+    return False
+
+def check_col():
+    for col in range(4):
+        if grid[0][col].__eq__(grid[1][col]) and grid[1][col].__eq__(grid[2][col]):
+            return True
+    return False
+
+def check_left_diag():
+    return grid[0][0].__eq__(grid[1][1]) and grid[1][1].__eq__(grid[2][2])
+
+def check_right_diag():
+    return grid[0][2].__eq__(grid[1][1]) and grid[1][1].__eq__(grid[2][0])
+
+def check_draw():
+    for row in range(4):
+        for col in range(4):
+            if grid[row][col].isnumeric():
+                return False  
+    return True
+
+def check_game_over():
+    if check_row():
+        print(current_piece + " wins!")
+        return True
+    elif check_col():
+        print(current_piece + " wins!")
+        return True
+    elif check_left_diag():
+        print(current_piece + " wins!")
+        return True
+    elif check_right_diag():
+        print(current_piece + " wins!")
+        return True
+    elif check_draw():
+        print("The Game Ends in a Draw!")
+        return True
+    else:
+        return False
+
+def game_loop():
+    global current_piece
+    print("Welcome to TIC TAC TOE")
+    user_choice = ""
+    while(True):
+        print_grid()
+        while(is_bad_choice_string(user_choice)):
+            user_choice = input("Enter STOP to end.  Or a number (1-9) where to put the piece: ")
+        if user_choice.__eq__("STOP"):
+            break
+        grid_spot = int(user_choice)
+        place_piece(grid_spot)
+        if(check_game_over()):
+            print_grid()
+            break
+        current_piece = "Y" if current_piece.__eq__("R") else "R"
+        user_choice = ""
+    print("GAME OVER")
 
 def game_loop():
     global current_piece
@@ -92,7 +172,7 @@ def game_loop():
     while(True):
         print_grid()
         while((user_choice)):
-            user_choice = input("Enter STOP to end.  Or a number (1 -42) where to put the piece: ")
+            user_choice = input("Enter STOP to end.  Or a number (1 - 42) where to put the piece: ")
         if user_choice.__eq__("STOP"):
             break
         grid_spot = int(user_choice)
@@ -100,7 +180,7 @@ def game_loop():
         if(check_game_over()):
             print_grid()
             break
-        current_piece = "O" if current_piece.__eq__("X") else "X"
+        current_piece = "Y" if current_piece.__eq__("R") else "R"
         user_choice = ""
     print("GAME OVER")
 
