a7a4aa3b01626da9a65de9884d5fd54b11fa432c
Gizzelle Harris <harrigiz000@warren.k12.in.us>
2024-05-02 08:17:07 -0400

    commit

--- 12_Game_Design_1/Connect4.py
+++ 12_Game_Design_1/Connect4.py
@@ -1,136 +1,140 @@
+from tkinter import *
+
+root = Tk()
+
+grid_color = "#003AFF"
+red_color = "#FF1800"
+yellow_color = "#FFF600"
+grid_width = 420
+grid_height = grid_width
+diameter = grid_width / 7
+total_rows = 6
+total_cols = 7
+
+screen = Canvas(root, width=grid_width, height=grid_height, bg=grid_color)
+screen.pack()
+
+
 grid = [
-    #1      2   3  4   5   6   7
-    ["1", "2","3","4","5","6","7"],
-    ["8", "9","10","11","12","13","14"],
-    ["15", "16","17","18","19","20","21"],
-    ["22", "23","24","25","26","27","28"],
-    ["29", "30","31","32","33","34","35"],
-    ["36", "37","38","39","40","41","42"]
+    ["-", "-", "-", "-", "-", "-", "-"],     # [R0C0, ..., R0C6]
+    ["-", "-", "-", "-", "-", "-", "-"],     # [R1C0, ..., R1C6]
+    ["-", "-", "-", "-", "-", "-", "-"],     # [R2C0, ..., R2C6]
+    ["-", "-", "-", "-", "-", "-", "-"],     # [R3C0, ..., R3C6]
+    ["-", "-", "-", "-", "-", "-", "-"],     # [R4C0, ..., R4C6]
+    ["-", "-", "-", "-", "-", "-", "-"]      # [R5C0, ..., R5C6]
 ]
 
-current_piece = "R", 
+current_piece = "R"
 
-col_header = ["1","2", "3", "4", "5", "6", "7"]
+last_row = -1
+last_col = -1
+remaining_spots = 42
 
-def print_grid():
-    for item in col_header:
-        print(item, " ")
-    print()
-    for row in range(len(grid)):
-        for col in range(len(grid[row])):
-            if col != 2:
-                print(grid[row][col], end="  ")            
-            else:
-                print(grid[row][col])
-                print()
+def draw_circle(row, col, color):
+    x_start = col * diameter
+    y_start = row * diameter
+    x_end = x_start + diameter
+    y_end = y_start + diameter
+    screen.create_oval(x_start, y_start, x_end, y_end, fill=color)
+    pass
 
-def is_bad_num_string(choice : str):
-    if (choice.isnumeric() and int(choice) >= 1 and int(choice) <= 9):
+
+def draw_grid():
+    for row in range(total_rows):
+        for col in range(total_cols):
+            draw_circle(row, col, "#FFFFFF")  
+
+def is_bad_num_string(choice: str):
+    if (choice.isnumeric() and int(choice) >= 0 and int(choice) <= 6):
         return False
     return True
 
-def get_row(grid_spot):
-    if grid_spot == 1 or grid_spot == 2 or grid_spot == 3:
-        return 0
-    elif grid_spot == 4 or grid_spot == 5 or grid_spot == 6:
-        return
-    elif grid_spot == 8 or grid_spot == 8 or grid_spot == 9:
-        return
-    elif grid_spot == 10 or grid_spot == 11 or grid_spot == 12:
-        return
-    elif grid_spot == 13 or grid_spot == 14 or grid_spot == 15:
-        return
-    elif grid_spot == 16 or grid_spot == 17 or grid_spot == 18:
-        return
-    elif grid_spot == 19 or grid_spot == 20 or grid_spot == 21:
-        return
-    elif grid_spot == 22 or grid_spot == 23 or grid_spot == 24:
-        return
-    elif grid_spot == 25 or grid_spot == 26 or grid_spot == 27:
-        return
-    elif grid_spot == 28 or grid_spot == 29 or grid_spot == 30:
-        return
-    elif grid_spot == 31 or grid_spot == 32 or grid_spot == 33:
-        return
-    elif grid_spot == 34 or grid_spot == 35 or grid_spot == 36:
-        return
-    elif grid_spot == 37 or grid_spot == 38 or grid_spot == 39:
-        return
-    elif grid_spot == 40 or grid_spot == 41 or grid_spot == 42:
-        return 
-    else:
-        return     
 
-def get_col(grid_spot):
-    if grid_spot == 1 or grid_spot == 2 or grid_spot == 3:
-        return 0
-    elif grid_spot == 4 or grid_spot == 5 or grid_spot == 6:
-        return
-    elif grid_spot == 8 or grid_spot == 8 or grid_spot == 9:
-        return
-    elif grid_spot == 10 or grid_spot == 11 or grid_spot == 12:
-        return
-    elif grid_spot == 13 or grid_spot == 14 or grid_spot == 15:
-        return
-    elif grid_spot == 16 or grid_spot == 17 or grid_spot == 18:
-        return
-    elif grid_spot == 19 or grid_spot == 20 or grid_spot == 21:
-        return
-    elif grid_spot == 22 or grid_spot == 23 or grid_spot == 24:
-        return
-    elif grid_spot == 25 or grid_spot == 26 or grid_spot == 27:
-        return
-    elif grid_spot == 28 or grid_spot == 29 or grid_spot == 30:
-        return
-    elif grid_spot == 31 or grid_spot == 32 or grid_spot == 33:
-        return
-    elif grid_spot == 34 or grid_spot == 35 or grid_spot == 36:
-        return
-    elif grid_spot == 37 or grid_spot == 38 or grid_spot == 39:
-        return
-    elif grid_spot == 40 or grid_spot == 41 or grid_spot == 42:
-        return 
-    else:
-        return     
+def is_bad_choice(choice: str):
+    if (choice.__eq__("STOP")):
+        return False
+    return is_bad_num_string(choice)
 
-def place_piece(grid_spot : int):
-    while(True):
-        row = get_row(grid_spot)
-        col = get_col(grid_spot)
-        grid_value = grid[row][col]
-        if (not grid_value.__eq__("R") and not grid_value.__eq__("Y")):
+
+def place_piece(col: int):
+    global last_row
+    global last_col
+    global remaining_spots
+    while (True):
+        row = 5
+        while (row >= 0):
+            if grid[row][col].__eq__("-"):
+                grid[row][col] = current_piece
+                last_row = row
+                last_col = col
+                remaining_spots -= 1
+                if current_piece.__eq__("R"):
+                    draw_circle(row, col, red_color)
+                else:
+                    draw_circle(row, col, yellow_color)
+                break
+            else:
+                row -= 1
+        if row != -1:
             break
-        user_choice = ""
-        while (is_bad_choice_string(user_choice)):
-            user_choice = input(
-                "Enter a number (1-42) where to put the piece: ")
-        grid_spot = int(user_choice)
-    grid[row][col] = current_piece 
+        else:
+            user_choice = ""
+            while (is_bad_num_string(user_choice)):
+                user_choice = input(
+                    "Enter a different number (0-6) where to drop the piece: ")
+            col = int(user_choice)
+
 
 def check_row():
-    for row in range(4):
-        if grid[row][0].__eq__(grid[row][1]) and grid[row][1].__eq__(grid[row][2]):
+    first_list = [last_col, last_col + 1, last_col + 2, last_col + 3]
+    second_list = [last_col - 1, last_col, last_col + 1, last_col + 2]
+    third_list = [last_col - 2, last_col - 1, last_col, last_col + 1]
+    fourth_list = [last_col - 3, last_col - 2, last_col - 1, last_col]
+    if(first_list[0] >= 0 and first_list[0] < 7 and first_list[3] >= 0 and first_list[3] < 7):
+        one = grid[last_row][first_list[0]]
+        two = grid[last_row][first_list[1]]
+        three = grid[last_row][first_list[2]]
+        four = grid[last_row][first_list[3]]
+        if one.__eq__(two) and two.__eq__(three) and three.__eq__(four):
+            return True
+    if (second_list[0] >= 0 and second_list[0] < 7 and second_list[3] >= 0 and second_list[3] < 7):
+        one = grid[last_row][second_list[0]]
+        two = grid[last_row][second_list[1]]
+        three = grid[last_row][second_list[2]]
+        four = grid[last_row][second_list[3]]
+        if one.__eq__(two) and two.__eq__(three) and three.__eq__(four):
+            return True
+    if (third_list[0] >= 0 and third_list[0] < 7 and third_list[3] >= 0 and third_list[3] < 7):
+        one = grid[last_row][third_list[0]]
+        two = grid[last_row][third_list[1]]
+        three = grid[last_row][third_list[2]]
+        four = grid[last_row][third_list[3]]
+        if one.__eq__(two) and two.__eq__(three) and three.__eq__(four):
+            return True
+    if (fourth_list[0] >= 0 and fourth_list[0] < 7 and fourth_list[3] >= 0 and fourth_list[3] < 7):
+        one = grid[last_row][fourth_list[0]]
+        two = grid[last_row][fourth_list[1]]
+        three = grid[last_row][fourth_list[2]]
+        four = grid[last_row][fourth_list[3]]
+        if one.__eq__(two) and two.__eq__(three) and three.__eq__(four):
             return True
     return False
-
+        
 def check_col():
-    for col in range(4):
-        if grid[0][col].__eq__(grid[1][col]) and grid[1][col].__eq__(grid[2][col]):
-            return True
-    return False
+    return False  # Actually Implement
+
 
 def check_left_diag():
-    return grid[0][0].__eq__(grid[1][1]) and grid[1][1].__eq__(grid[2][2])
+    return False  # Actually Implement
+
 
 def check_right_diag():
-    return grid[0][2].__eq__(grid[1][1]) and grid[1][1].__eq__(grid[2][0])
+    return False  # Actually Implement
+
 
 def check_draw():
-    for row in range(4):
-        for col in range(4):
-            if grid[row][col].isnumeric():
-                return False  
-    return True
+    return remaining_spots == 0
+
 
 def check_game_over():
     if check_row():
@@ -151,23 +155,35 @@ def check_game_over():
     else:
         return False
 
+
 def game_loop():
     global current_piece
-    print("LET'S PLAY CONNECT 4!")
+    print("Welcome to CONNECT FOUR")
     user_choice = ""
-    while(True):
-        print_grid()
-        while((user_choice)):
-            user_choice = input("Enter STOP to end.  Or a number (1 - 7) where to put the piece: ")
+    draw_grid()
+    while (True):
+        while (is_bad_choice(user_choice)):
+            user_choice = input(
+                "Enter STOP to end.  Or a number (0-6) where to drop the piece: ")
         if user_choice.__eq__("STOP"):
             break
-        grid_spot = int(user_choice)
-        place_piece(grid_spot)
-        if(check_game_over()):
-            print_grid()
+        column = int(user_choice)
+        place_piece(column)
+        if (check_game_over()):
             break
         current_piece = "Y" if current_piece.__eq__("R") else "R"
         user_choice = ""
     print("GAME OVER")
 
-game_loop()          
+**********************
+def game_loop():
+    global current_piece
+    print("Welcome to Connect 4!")
+    draw_grid()
+    while(True):
+        id(mouse_clicked):
+        place_piece(user_choice_col)
+        mouse_clicked = False
+game_loop()
+
+mainloop()
