810b7655df5904433930bb9d89a9d693076e6a7e
Gizzelle Harris <harrigiz000@warren.k12.in.us>
2024-05-09 08:15:31 -0400

    commit

---
+++ 12_Game_Design_1/Final_Project/Day2.py
@@ -0,0 +1,79 @@
+import tkinter as tk
+
+class Game(tk.Frame):
+    def __init__(self, master):
+        super(Game, self).__init__(master)
+        self.lives = 3
+        self.width = 600
+        self.height = 400
+        self.bg = "#AAAAFF"
+        self.canvas = tk.Canvas(self, width=self.width, height=self.height, bg=self.bg)        
+        self.canvas.pack()
+        self.pack()
+
+class GameObject(object):
+    def __init__(self, canvas, item):
+        self.canvas = canvas
+        self.item = item
+
+    def get_position(self):
+        return self.canvas.coords(self.item)
+    
+    def move(self, x, y):
+        self.canvas.move(self.item, x, y)
+
+    def delete(self):
+        self.canvas.delete(self.item)
+
+    def update(self):
+
+
+class Ball(GameObject):
+    def __init__(self, canvas, x, y):  
+        self.radius = 10 
+        self.direction = [1, -1]
+        #This represents the speed of the ball in the x and y direction.  Example:  [xspeed, yspeed]  
+        self.speed = 10 
+        x1 = x - self.radius
+        y1 = y - self.radius
+        x2 = x + self.radius
+        y2 =  y + self.radius  
+        color = white
+        item = canvas.create_oval(x1, y1, x2, y2, fill=color)
+        super(Ball, self).__init__(canvas, item)
+
+class Paddle(GameObject):
+    def __init__(self, canvas, x, y): 
+        self.width = 80
+        self.height = 10
+        self.ball = None
+        x1 = x - self.width / 2
+        y1 = self.height / 2 
+        x2 = x + self.width / 2 
+        y2 = y + self.height / 2
+        color = blue 
+        ###canvas.create_rectangle =  x1,y1, x2, y2, color
+        super(Paddle, self).__init__(canvas, item)
+        
+    def set_ball(self, ball):
+        self.ball = ball
+
+    def move(self, offset):
+        coords = self.get_position()
+        width = self.canvas.winfo.width()
+        x1 = coords[0]
+        y1 = coords[1]
+        x2 = coords[2]
+        y2 = coords[3]
+        if x1 + offset >= 0 and x2 + offset <= width:
+            super(Paddle, self).move(offset, 0)
+        if self.ball is not None:
+            self.ball.move(offset, 0)
+
+
+
+if __name__ == "__main__":    
+    root = tk.Tk()
+    game = Game(root)
+    root.title("BREAKOUT")
+    root.mainloop()
